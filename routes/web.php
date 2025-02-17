f4810900260be1e4b76d078caee3488461a08ec8
RamirezDei <ramirezdeivi14@gmail.com>
2025-02-16 22:47:24 -0500

    Crear Controlador y Rutas

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+
+use App\Http\Controllers\CocktailController;
+
+Route::get('/', [CocktailController::class, 'index']);
+Route::post('/save', [CocktailController::class, 'store']);
+Route::get('/saved', [CocktailController::class, 'showSaved']);
+Route::post('/delete/{id}', [CocktailController::class, 'destroy']);
+
